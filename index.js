bf4090c9b3177730705f5e43a4e9cc7f3a35167e
mikeryu98 <mikeryu98@gmail.com>
2025-12-28 18:35:00 +0900

    v1.0.1 - CORS 해결

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const cors = require('cors');
 
+const PLUGIN_NAME = 'homebridge-blog-light';
+const PLATFORM_NAME = 'BlogLightPlatform';
+
 module.exports = (api) => {
-    api.registerPlatform('BlogLightPlatform', BlogLightPlatform);
+    api.registerPlatform(PLATFORM_NAME, BlogLightPlatform);
 };
 
 class BlogLightPlatform {
     constructor(log, config, api) {
         this.log = log;
-        this.config = config;
         this.api = api;
+
+        this.config = config || {};
+        this.port = this.config.port || 8000;
+        this.accessoryName = this.config.name || "Blog Light";
 
         this.brightness = 10;
         this.Service = api.hap.Service;
@@ -17,10 +23,11 @@ class BlogLightPlatform {
 
         this.accessories = [];
 
+        // 서버 시작
         this.startServer();
 
         this.api.on('didFinishLaunching', () => {
-            this.log.info('블로그 조명 플랫폼 로딩 완료');
+            this.log.info(`${this.accessoryName} 플랫폼 로딩 완료`);
             this.setupAccessory();
         });
     }
@@ -31,36 +38,34 @@ class BlogLightPlatform {
     }
 
     setupAccessory() {
-        const name = "Blog Light";
-        const uuid = this.api.hap.uuid.generate('homebridge:blog-light');
-
+        const uuid = this.api.hap.uuid.generate('homebridge:blog-light-' + this.accessoryName);
         const existingAccessory = this.accessories.find(acc => acc.UUID === uuid);
 
         if (existingAccessory) {
-            this.log.info('기존 액세서리 사용');
+            this.log.info(`기존 액세서리 사용: ${this.accessoryName}`);
             this.linkService(existingAccessory);
         } else {
-            this.log.info('새 액세서리 생성 중...');
-            const accessory = new this.api.platformAccessory(name, uuid);
-            accessory.addService(this.Service.Lightbulb, name);
+            this.log.info(`새 액세서리 생성 중: ${this.accessoryName}`);
+            const accessory = new this.api.platformAccessory(this.accessoryName, uuid);
+            accessory.addService(this.Service.Lightbulb, this.accessoryName);
             this.linkService(accessory);
 
-            this.api.registerPlatformAccessories('homebridge-blog-light', 'BlogLightPlatform', [accessory]);
+            this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
         }
     }
 
     linkService(accessory) {
-        const service = accessory.getService(this.Service.Lightbulb);
+        const service = accessory.getService(this.Service.Lightbulb) || accessory.addService(this.Service.Lightbulb);
 
         service.getCharacteristic(this.Characteristic.On)
             .onGet(() => true)
-            .onSet((val) => this.log.info('전원:', val));
+            .onSet((val) => this.log.info(`${this.accessoryName} 전원:`, val));
 
         service.getCharacteristic(this.Characteristic.Brightness)
             .onGet(() => this.brightness)
             .onSet((val) => {
                 this.brightness = val;
-                this.log.info(`조도 변경: ${val}%`);
+                this.log.info(`${this.accessoryName} 조도 변경: ${val}%`);
             });
     }
 
@@ -74,16 +79,11 @@ class BlogLightPlatform {
         }));
 
         app.get('/light', (req, res) => {
-            res.setHeader('Access-Control-Allow-Origin', '*');
-            res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-            res.setHeader('Access-Control-Allow-Headers', 'Content-Type, ngrok-skip-browser-warning');
-            res.setHeader('ngrok-skip-browser-warning', 'true');
-
             res.json({ brightness: this.brightness });
         });
 
-        app.listen(8000, '0.0.0.0', () => {
-            this.log.info('API 서버 실행 중: 8010');
+        app.listen(this.port, '0.0.0.0', () => {
+            this.log.info(`API 서버 실행 중: 포트 ${this.port}`);
         });
     }
 }
